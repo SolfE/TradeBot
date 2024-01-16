b734e0b80d144a056d37e3719f0651dbf311f6eb
SolfE <love23869318@gmail.com>
2024-01-16 14:32:08 +0900

    fix: remove chk_event_2

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 from log import Log
 from request import get_mark_price
-from event import chk_event_1, chk_event_2
+from event import chk_event_1
 from discordAPI import send_msg
 from config import SYMBOL_LIST, SERVER_INTERVAL
 
@@ -21,10 +21,10 @@ while True:
                 log = Log("ET1", "알림발생", symbol)
                 log.lprint()
                 send_msg(log.get())
-            if chk_event_2(response):
-                log = Log("ET2", "알림발생", symbol)
-                log.lprint()
-                send_msg(log.get())
+            # if chk_event_2(response):
+            #     log = Log("ET2", "알림발생", symbol)
+            #     log.lprint()
+            #     send_msg(log.get())
 
             # df = response
             # df.plot(x="time", y=["C", "50EMA", "100EMA"])
